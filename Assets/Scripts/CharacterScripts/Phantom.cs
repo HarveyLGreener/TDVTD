32bf461b09e22edce6727095f8a399c5f039d996
aleenagraveline <124313468+aleenagraveline@users.noreply.github.com>
2024-04-07 21:56:51 -0400

    phantom smoke screen

---
+++ Assets/Scripts/CharacterScripts/Phantom.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Phantom : MonoBehaviour
+{
+    [SerializeField] private GameObject smokeScreen;
+    [SerializeField] private bool activeCountdown = false;
+    [SerializeField] private float smokeScreenLength = 0.75f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!activeCountdown && Input.GetKeyDown(KeyCode.K))
+        {
+            smokeScreen.SetActive(true);
+            if(!activeCountdown)
+            {
+                StartCoroutine(SmokeScreenActive());
+            }
+        }
+    }
+
+    public IEnumerator SmokeScreenActive()
+    {
+        activeCountdown = true;
+        yield return new WaitForSeconds(smokeScreenLength);
+        smokeScreen.SetActive(false);
+        yield return new WaitForSeconds(3.0f);
+        activeCountdown = false;
+    }
+}
---
+++ Assets/Scripts/CharacterScripts/SmokeScreen.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeScreen : MonoBehaviour
+{
+    [SerializeField] private float pushForce = 3f;
+    [SerializeField] private GameObject phantom;
+    [SerializeField] private GameObject hitOpp;
+    [SerializeField] private bool oppMovementDisabled = false;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Rigidbody2D rb = col.GetComponent<Rigidbody2D>();
+        if (rb != null && rb != phantom.GetComponent<Rigidbody2D>())
+        {
+            if (!oppMovementDisabled)
+            {
+                hitOpp = col.gameObject;
+
+                if(hitOpp.transform.position.x > phantom.transform.position.x)
+                {
+                    rb.AddForce(Vector3.right * pushForce, ForceMode2D.Impulse);
+                }
+                else
+                {
+                    rb.AddForce(Vector3.left * pushForce, ForceMode2D.Impulse);
+                }
+
+                StartCoroutine(RattlesDisableMove());
+            }
+        }
+    }
+
+    public IEnumerator RattlesDisableMove()
+    {
+        hitOpp.GetComponent<PlayerMovement>().enabled = false;
+        oppMovementDisabled = true;
+        yield return new WaitForSeconds(0.5f);
+        hitOpp.GetComponent<PlayerMovement>().enabled = true;
+        oppMovementDisabled = false;
+    }
+}
