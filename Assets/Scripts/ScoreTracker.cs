fd55dfaff9dc663cae5ace8cfe130f0c43247c3b
HarveyLGreener <therebelrangerlemonpop@gmail.com>
2024-04-18 14:22:02 -0400

    Score should now reset after reaching a "firstTo" variable.

--- Assets/Scripts/ScoreTracker.cs
+++ Assets/Scripts/ScoreTracker.cs
@@ -9,15 +9,18 @@ public class ScoreTracker : MonoBehaviour
     [SerializeField] private TextMeshProUGUI player2;
     private int player1Score = 0;
     private int player2Score = 0;
+    public int firstTo;
     // Start is called before the first frame update
 
     public void PhantomPoint()
     {
         player1Score += 1;
         player1.text ="" + player1Score;
-        if (player1Score >= 3)
+        if (player1Score >= firstTo)
         {
             Debug.Log("Phantom Wins!");
+            player1Score = 0;
+            player2Score = 0;
         }
     }
 
@@ -25,9 +28,11 @@ public class ScoreTracker : MonoBehaviour
     {
         player2Score += 1;
         player2.text = "" + player2Score;
-        if (player2Score >= 3)
+        if (player2Score >= firstTo)
         {
             Debug.Log("Rattles Wins!");
+            player1Score = 0;
+            player2Score = 0;
         }
     }
 
